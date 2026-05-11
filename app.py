818f677ab7722d969ecc1d2c6406c43f334e250e
muzammikhan92512-del <muzammilkhan92512@gmail.com>
2026-05-11 09:18:23 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Hello World from Flask Docker!</h1>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "<h1>Hello World from Flask Docker!</h1>"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
